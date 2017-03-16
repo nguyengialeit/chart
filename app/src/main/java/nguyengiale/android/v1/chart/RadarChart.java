7af0799659b2e4b308de5b3fe4e46febaf97d38c
giale@trycz.vn <giale@trycz.vn>
2017-03-16 15:44:47 +0700

    more feature for radar chart

--- app/src/main/java/nguyengiale/android/v1/chart/RadarChart.java
+++ app/src/main/java/nguyengiale/android/v1/chart/RadarChart.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -16,6 +17,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import nguyengiale.android.v1.chart.object.PointLabel;
 
@@ -30,12 +32,14 @@ public class RadarChart extends View {
     private float mCenterX, mCenterY;
     private ArrayList<String> mListColor = new ArrayList<>();
     private float mRadius, mTitleHeight, mPointHeight, mPadding;
-    private ArrayList<Integer> mListOldPoint = new ArrayList<>();
+    private ArrayList<Float> mListOldPoint = new ArrayList<>();
     private ArrayList<PointLabel> mListPoint = new ArrayList<>();
     private ArrayList<PointLabel> mListPointTemp = new ArrayList<>();
-    private int mMaxPoint;
+    private float mMaxPoint;
     private Point[] mListPolygonPoint = new Point[6];
     private Point[] mListPolygonOldPoint = new Point[6];
+    private List<String> mListLabel = new ArrayList<>();
+    private int mPaddingLabel = 20;
 
     public RadarChart(final Context context, final AttributeSet attrs) {
         super(context, attrs);
@@ -87,10 +91,11 @@ public class RadarChart extends View {
             if (!mListOldPoint.isEmpty())
                 for (int i = 0; i < mListOldPoint.size(); i++) {
                     if (mListOldPoint.get(i) == 0)
-                        mListOldPoint.set(i, ((int) (mMaxPoint * 0.1)));
+                        mListOldPoint.set(i, (mMaxPoint * 0.1f));
                     else
                         mListOldPoint.set(i, mListOldPoint.get(i) / 30 * mMaxPoint);
                 }
+
 
             Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
             Canvas tempCanvas = new Canvas(bitmap);
@@ -210,8 +215,8 @@ public class RadarChart extends View {
                     float xB = -(mCenterY - mRadius * mListOldPoint.get(i) / mMaxPoint - mCenterY) * (float) Math.sin(Math.toRadians(i * 60)) + mCenterX;
                     float yB = (mCenterY - mRadius * mListOldPoint.get(i) / mMaxPoint - mCenterY) * (float) Math.cos(Math.toRadians(i * 60)) + mCenterY;
 
-                    mListPolygonPoint[i] = new Point(xA, yA);
-                    mListPolygonOldPoint[i] = new Point(xB, yB);
+                    mListPolygonPoint[i] = new Point((int) xA, (int) yA);
+                    mListPolygonOldPoint[i] = new Point((int) xB, (int) yB);
                 }
             else for (int i = 0; i < 6; i++) {
                 mPaint.setColor(Color.parseColor(mListColor.get(i)));
@@ -219,30 +224,29 @@ public class RadarChart extends View {
                 float xA = -(mCenterY - mRadius * mListPoint.get(i).getPoint() / mMaxPoint - mCenterY) * (float) Math.sin(Math.toRadians(i * 60)) + mCenterX;
                 float yA = (mCenterY - mRadius * mListPoint.get(i).getPoint() / mMaxPoint - mCenterY) * (float) Math.cos(Math.toRadians(i * 60)) + mCenterY;
 
-                mListPolygonPoint[i] = new Point(xA, yA);
+                mListPolygonPoint[i] = new Point((int) xA, (int) yA);
             }
 
             canvas.save();
             canvas.drawBitmap(bitmap, 0, 0, null);
             canvas.restore();
 
-
+            if (mListLabel.size() >= 3) {
+                canvas.save();
+                canvas.drawBitmap(maskingImage(drawBorder(), drawLine()), 0, 0, null);
+                canvas.restore();
+            }
             Bitmap bitmap2 = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
             Canvas tempCanvas2 = new Canvas(bitmap2);
             drawPoly(tempCanvas2, getResources().getColor(android.R.color.white), mListPolygonPoint);
-
             Bitmap original = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getContext().getResources(), R.drawable.background_radar_chart), mWidth, mHeight, false);
-            Bitmap mask = bitmap2;
 
-            //You can change original image here and draw anything you want to be masked on it.
+            canvas.save();
+            canvas.drawBitmap(maskingImage(original, bitmap2), 0, 0, null);
+            canvas.restore();
 
-            Bitmap result = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
+            Bitmap result = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
             Canvas tempCanvas3 = new Canvas(result);
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-            tempCanvas3.drawBitmap(original, 0, 0, null);
-            tempCanvas3.drawBitmap(mask, 0, 0, paint);
-            paint.setXfermode(null);
 
             mPaint.setColor(Color.argb(30, 0, 0, 0));
             mPaint.setStrokeWidth(6);
@@ -272,6 +276,11 @@ public class RadarChart extends View {
                 float yA = (mCenterY - mRadius * mListPoint.get(i).getPoint() / mMaxPoint - mCenterY) * (float) Math.cos(Math.toRadians(i * 60)) + mCenterY;
                 tempCanvas3.drawCircle(xA, yA, mWidth / 105, mPaint);
             }
+            if (mListLabel.size() >= 3) {
+                canvas.save();
+                canvas.drawBitmap(drawLabel(), 0, 0, null);
+                canvas.restore();
+            }
         } else {
             Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
             Canvas tempCanvas = new Canvas(bitmap);
@@ -284,17 +293,6 @@ public class RadarChart extends View {
             canvas.save();
             canvas.drawBitmap(bitmap, 0, 0, null);
             canvas.restore();
-        }
-    }
-
-    class Point {
-
-        public float x = 0;
-        public float y = 0;
-
-        public Point(float x, float y) {
-            this.x = x;
-            this.y = y;
         }
     }
 
@@ -321,13 +319,186 @@ public class RadarChart extends View {
         canvas.drawPath(polyPath, polyPaint);
     }
 
+    private Bitmap drawLabel() {
+        Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(bitmap);
+        Point[] points = new Point[6];
+        for (int i = 0; i < 6; i++) {
+            float xA = -(mCenterY - mRadius - mCenterY) * (float) Math.sin(Math.toRadians(i * 60)) + mCenterX;
+            float yA = (mCenterY - mRadius - mCenterY) * (float) Math.cos(Math.toRadians(i * 60)) + mCenterY;
+
+            points[i] = new Point((int) xA, (int) yA);
+        }
+        Paint mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(3);
+
+        Rect bounds = new Rect();
+        mPaint.setTextSize(mWidth / 22f);
+
+        //label 1
+        mPaint.getTextBounds(mListLabel.get(0), 0, mListLabel.get(0).length(), bounds);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.parseColor("#ffffff"));
+        mPaint.setAlpha(150);
+        tempCanvas.drawRect(mWidth - mPaddingLabel * 2 - bounds.width(), points[1].y + mPaddingLabel, mWidth, points[1].y + mPaddingLabel * 3 + bounds.height(), mPaint);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.parseColor(mListColor.get(1)));
+        mPaint.setAlpha(255);
+        tempCanvas.drawRect(mWidth - mPaddingLabel * 2 - bounds.width(), points[1].y + mPaddingLabel, mWidth, points[1].y + mPaddingLabel * 3 + bounds.height(), mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLACK);
+        tempCanvas.drawText(mListLabel.get(0), mWidth - mPaddingLabel - bounds.width(), points[1].y + mPaddingLabel * 2 + bounds.height(), mPaint);
+
+        //label 2
+        mPaint.getTextBounds(mListLabel.get(1), 0, mListLabel.get(1).length(), bounds);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.parseColor("#ffffff"));
+        mPaint.setAlpha(150);
+        tempCanvas.drawRect(mWidth / 2 - bounds.width() / 2 - mPaddingLabel, points[3].y - mPaddingLabel * 7 - bounds.height(), mWidth / 2 + bounds.width() / 2 + mPaddingLabel, points[3].y - mPaddingLabel * 5, mPaint);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.parseColor(mListColor.get(3)));
+        mPaint.setAlpha(255);
+        tempCanvas.drawRect(mWidth / 2 - bounds.width() / 2 - mPaddingLabel, points[3].y - mPaddingLabel * 7 - bounds.height(), mWidth / 2 + bounds.width() / 2 + mPaddingLabel, points[3].y - mPaddingLabel * 5, mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLACK);
+        tempCanvas.drawText(mListLabel.get(1), mWidth / 2 - bounds.width() / 2, points[3].y - mPaddingLabel * 6, mPaint);
+
+        //label 3
+        mPaint.getTextBounds(mListLabel.get(2), 0, mListLabel.get(2).length(), bounds);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.parseColor("#ffffff"));
+        mPaint.setAlpha(150);
+        tempCanvas.drawRect(0, points[5].y + mPaddingLabel, bounds.width() + mPaddingLabel * 2, points[5].y + mPaddingLabel * 3 + bounds.height(), mPaint);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.parseColor(mListColor.get(5)));
+        mPaint.setAlpha(255);
+        tempCanvas.drawRect(0, points[5].y + mPaddingLabel, bounds.width() + mPaddingLabel * 2, points[5].y + mPaddingLabel * 3 + bounds.height(), mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLACK);
+        tempCanvas.drawText(mListLabel.get(2), mPaddingLabel, points[5].y + mPaddingLabel * 2 + bounds.height(), mPaint);
+
+        return bitmap;
+    }
+
+    private Bitmap drawLine() {
+        float a = (float) Math.sqrt(Math.pow(mWidth, 2) + Math.pow(mHeight, 2));
+        Bitmap bitmap = Bitmap.createBitmap((int) a, (int) a, Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(bitmap);
+        Paint mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(3);
+        tempCanvas.translate((mWidth - a) / 2, (mHeight - a) / 2);
+        tempCanvas.rotate(-45, a / 2, a / 2);
+        for (int i = 0; i < a; i++) {
+            if (i % 7 == 0) {
+                mPaint.setAlpha(255);
+                tempCanvas.drawLine(i, 0, i, a, mPaint);
+            } else {
+                mPaint.setAlpha(20);
+                tempCanvas.drawLine(i, 0, i, a, mPaint);
+            }
+        }
+        return bitmap;
+    }
+
+    private Bitmap drawBorder() {
+        float a = (float) Math.sqrt(Math.pow(mWidth, 2) + Math.pow(mHeight, 2));
+        Bitmap bitmap = Bitmap.createBitmap((int) a, (int) a, Bitmap.Config.ARGB_8888);
+        Point[] points = new Point[6];
+        for (int i = 0; i < 6; i++) {
+            float xA = -(mCenterY - mRadius - mCenterY) * (float) Math.sin(Math.toRadians(i * 60)) + mCenterX;
+            float yA = (mCenterY - mRadius - mCenterY) * (float) Math.cos(Math.toRadians(i * 60)) + mCenterY;
+
+            points[i] = new Point((int) xA, (int) yA);
+        }
+
+        Paint mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(2);
+
+        Canvas tempCanvas1 = new Canvas(bitmap);
+        Path polyPath1 = new Path();
+        mPaint.setColor(Color.parseColor(mListColor.get(1)));
+        polyPath1.moveTo(mListPolygonPoint[0].x, mListPolygonPoint[0].y);
+        polyPath1.lineTo(mListPolygonPoint[0].x, mListPolygonPoint[0].y);
+        polyPath1.lineTo(points[0].x, points[0].y);
+        polyPath1.lineTo(points[1].x, points[1].y);
+        polyPath1.lineTo(points[2].x, points[2].y);
+        polyPath1.lineTo(mListPolygonPoint[2].x, mListPolygonPoint[2].y);
+        polyPath1.lineTo(mListPolygonPoint[1].x, mListPolygonPoint[1].y);
+        polyPath1.lineTo(mListPolygonPoint[0].x, mListPolygonPoint[0].y);
+        tempCanvas1.drawPath(polyPath1, mPaint);
+
+        Canvas tempCanvas2 = new Canvas(bitmap);
+        Path polyPath2 = new Path();
+        mPaint.setColor(Color.parseColor(mListColor.get(3)));
+        polyPath2.moveTo(mListPolygonPoint[2].x, mListPolygonPoint[2].y);
+        polyPath2.lineTo(mListPolygonPoint[2].x, mListPolygonPoint[2].y);
+        polyPath2.lineTo(points[2].x, points[2].y);
+        polyPath2.lineTo(points[3].x, points[3].y);
+        polyPath2.lineTo(points[4].x, points[4].y);
+        polyPath2.lineTo(mListPolygonPoint[4].x, mListPolygonPoint[4].y);
+        polyPath2.lineTo(mListPolygonPoint[3].x, mListPolygonPoint[3].y);
+        polyPath2.lineTo(mListPolygonPoint[2].x, mListPolygonPoint[2].y);
+        tempCanvas2.drawPath(polyPath2, mPaint);
+
+        Canvas tempCanvas3 = new Canvas(bitmap);
+        Path polyPath3 = new Path();
+        mPaint.setColor(Color.parseColor(mListColor.get(5)));
+        polyPath3.moveTo(mListPolygonPoint[4].x, mListPolygonPoint[4].y);
+        polyPath3.lineTo(mListPolygonPoint[4].x, mListPolygonPoint[4].y);
+        polyPath3.lineTo(points[4].x, points[4].y);
+        polyPath3.lineTo(points[5].x, points[5].y);
+        polyPath3.lineTo(points[0].x, points[0].y);
+        polyPath3.lineTo(mListPolygonPoint[0].x, mListPolygonPoint[0].y);
+        polyPath3.lineTo(mListPolygonPoint[5].x, mListPolygonPoint[5].y);
+        polyPath3.lineTo(mListPolygonPoint[4].x, mListPolygonPoint[4].y);
+        tempCanvas3.drawPath(polyPath3, mPaint);
+        return bitmap;
+    }
+
+    private Bitmap maskingImage(Bitmap s1, Bitmap s2) {
+        Bitmap original = s1;
+        Bitmap mask = s2;
+        Bitmap result = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas mCanvas = new Canvas(result);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setAlpha((int) (0.7 * 255));
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        mCanvas.drawBitmap(original, 0, 0, null);
+        mCanvas.drawBitmap(mask, 0, 0, paint);
+        paint.setXfermode(null);
+        return result;
+    }
+
     public void setListPoint(ArrayList<PointLabel> pListPointLabel) {
         this.mListPointTemp = pListPointLabel;
         this.invalidate();
     }
 
-    public void setListOldPoint(ArrayList<Integer> pListPointLabel) {
+    public void setListOldPoint(ArrayList<Float> pListPointLabel) {
         this.mListOldPoint = pListPointLabel;
         this.invalidate();
+    }
+
+    public List<String> getmListLabel() {
+        return mListLabel;
+    }
+
+    public void setmListLabel(List<String> mListLabel) {
+        this.mListLabel = mListLabel;
+        invalidate();
     }
 }
